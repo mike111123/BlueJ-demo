77a946885457e7b5203daa7c3bacdebfdeb0428a
mike <michaelredding2000@gmail.com>
2021-02-09 14:33:14 -0500

    Initial sharing of project

---
+++ Newclass1.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Newclass1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Newclass1
+{
+    public static void main(String []args) {
+      System.out.println("Hello World"); // prints Hello World
+   }
+}
+
+
